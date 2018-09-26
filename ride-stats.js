93453811139ed4075de9f302602661ba72012aa7
idoco <ido.c.mail@gmail.com>
2018-09-26 19:20:26 +0300

    add csv stats

--- ride-stats.js
+++ ride-stats.js
@@ -1,70 +1,60 @@
+const fs = require('fs');
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+
 const moment = require("moment");
 const momentDurationFormatSetup = require("moment-duration-format");
-const geodist = require('geodist');
 
 const createTimeline = require('./timeline').createTimeline;
 const collectBirdLocations = require('./timeline').collectBirdLocations;
+const collectBirdRides = require('./timeline').collectBirdRides;
 
-const targetFolder = process.argv[2] || './data/tlv_9_12_2018';
+const targetFolder = process.argv[2] || './data/tlv_14_2018';
+const statsType = process.argv[3] || 'text'; // can be csv
 
 const main = async () => {
 
-    const timeline = await createTimeline(targetFolder);
+    switch (statsType) {
+
+        case 'text':
+            const rideStats = await collectRidesStats(targetFolder);
+            // console.log(JSON.stringify(rideStats.rides, null, 4));    
+            printStats(rideStats);
+            break;
+
+        case 'csv':
+            // targetFolder expects '/' sufix
+            let dataFolders = (await readdir(targetFolder)).sort().filter(filename => !filename.startsWith('.'));
+            dataFolders = dataFolders.map(folderName => targetFolder + folderName)
+            const multipleRideStats = await Promise.all(dataFolders.map(collectRidesStats));
+            printCsv(multipleRideStats);
+            break;
+
+        case 'histogram':
+            // expects specifc day folder
+            const histogram = await getHistogram(targetFolder);
+            console.log(histogram);
+            break;
+
+    }
+
+}
+
+const collectRidesStats = async (folder) => {
+    const timeline = await createTimeline(folder);
     const birdLocations = collectBirdLocations(timeline);
     const rides = collectBirdRides(birdLocations);
 
     const sortedRides = rides.sort((a, b) => a.duration - b.duration);
-    const middle = Math.floor(sortedRides.length/2)
+    const middle = Math.floor(sortedRides.length / 2)
     const medianDuration = moment.duration(sortedRides[middle].duration, "milliseconds").format("hh:mm:ss")
-    
-    // console.log(JSON.stringify(sortedRides, null, 4))
 
     const numberOfRides = rides.length;
     const totals = getTotals(rides);
     const averages = getAverages(numberOfRides, totals);
-    console.log('Stats for', targetFolder);
-    console.log('number of birds spotted', Object.keys(birdLocations).length);
-    console.log('number of rides', rides.length);
-    console.log('revenue in NIS', totals.shekels);
-    console.log('median ride duration', medianDuration);
-    console.log('averages', JSON.stringify(averages, null, 4));
 
-}
-
-// list of all bird rides (That are longer than x meters)
-const collectBirdRides = (birdsLocations) => {
-
-    const rides = [];
-
-    Object.keys(birdsLocations).forEach(birdCode => {
-        const currentBirdLocations = birdsLocations[birdCode];
-        currentBirdLocations.forEach((birdStep, stepIndex) => {
-            if (stepIndex == 0) return; // first step can't end a ride, skip it
-
-            const lastBirdStep = currentBirdLocations[stepIndex - 1];
-            const distance = distanceInMeters(lastBirdStep.location, birdStep.location);
-            const batteryUsed = lastBirdStep.battery_level - birdStep.battery_level;
-            const duration = birdStep.date - lastBirdStep.date;
-            const batteryUsedPerHour = batteryUsed / (duration / 60 / 60 / 1000);
-
-            // If the new bird location qualifies as a ride add it to the list
-            if (distance > 100 && duration > (2 * 60 * 1000) && batteryUsed > 0 && batteryUsedPerHour > 1) {
-                rides.push({
-                    birdCode, distance, duration, batteryUsed,
-                    formatedDuration: moment.duration(duration, "milliseconds").format("hh:mm:ss"),
-                    startTime: new Date(lastBirdStep.date).toString(),
-                    endTime: new Date(birdStep.date).toString(),
-                    startLocation: lastBirdStep.location,
-                    endLocation: birdStep.location,
-                    battery_level: birdStep.battery_level,
-                    shekels: 5 + (0.5 * Math.ceil(duration / 60000)),
-                    usd: 1 + (0.2 * Math.ceil(duration / 60000))
-                });
-            }
-        });
-    });
-
-    return rides;
+    return { folder, birdLocations, rides, medianDuration, totals, averages };
 }
 
 const getTotals = (rides) => rides.reduce((a, b) => ({
@@ -79,16 +69,42 @@ const getAverages = (numberOfRides, totals) => ({
     duration: moment.duration(totals.duration / numberOfRides, "milliseconds").format("hh:mm:ss"),
     batteryUsed: Number((totals.batteryUsed / numberOfRides).toFixed(2)),
     shekels: Number((totals.shekels / numberOfRides).toFixed(2)),
-    powerUsedPerKm: Number((1000 / (totals.distance / totals.batteryUsed)).toFixed(2)) 
-}); 
+    powerUsedPerKm: Number((1000 / (totals.distance / totals.batteryUsed)).toFixed(2))
+});
 
-const distanceInMeters = (p1, p2) => {
-        return geodist(
-            { lat: p1.latitude, lon: p1.longitude },
-            { lat: p2.latitude, lon: p2.longitude },
-            { unit: 'meters' }
-        );
+const printStats = ({ folder, birdLocations, rides, medianDuration, totals, averages }) => {
+    console.log('Stats for', folder);
+    console.log('number of birds spotted', Object.keys(birdLocations).length);
+    console.log('number of rides', rides.length);
+    console.log('revenue in NIS', totals.shekels);
+    console.log('median ride duration', medianDuration);
+    console.log('averages', JSON.stringify(averages, null, 4));
+};
 
-    }
 
-    main();
+const printCsv = (rideStats) => {
+    console.log("date, birds_spotted, number_of_rides, totals_shekels, median_duration, avg_distance, avg_duration, avg_batteryUsed, avg_shekels, powerUsedPerKm");
+    rideStats.forEach(line => {
+        console.log(toCsvLine(line));
+    })
+}
+
+const toCsvLine = ({ folder, birdLocations, rides, medianDuration, totals, averages }) => [
+    folder, Object.keys(birdLocations).length, rides.length, totals.shekels, medianDuration,
+    ...Object.values(averages)
+].join(", ");
+
+const getHistogram = async (folder) => {
+    const timeline = await createTimeline(folder);
+    const birdLocations = collectBirdLocations(timeline);
+    const rides = collectBirdRides(birdLocations);
+
+    const histogram = {};
+    rides.forEach(ride => {
+        const rideHour = ride.startTime.getHours();
+        histogram[rideHour] = histogram[rideHour] ? histogram[rideHour] + 1 : 1;
+    });
+    return histogram;
+}
+
+main();
--- timeline.js
+++ timeline.js
@@ -4,6 +4,10 @@ const util = require('util');
 const readdir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
 
+const moment = require("moment");
+const momentDurationFormatSetup = require("moment-duration-format");
+const geodist = require('geodist');
+
 // read data folder and create full timeline
 const createTimeline = async (dataFolder) => {
 
@@ -35,7 +39,52 @@ const collectBirdLocations = (timeline) => {
     return birdsLocations;
 }
 
+// list of all bird rides (That are longer than x meters)
+const collectBirdRides = (birdsLocations) => {
+
+    const rides = [];
+
+    Object.keys(birdsLocations).forEach(birdCode => {
+        const currentBirdLocations = birdsLocations[birdCode];
+        currentBirdLocations.forEach((birdStep, stepIndex) => {
+            if (stepIndex == 0) return; // first step can't end a ride, skip it
+
+            const lastBirdStep = currentBirdLocations[stepIndex - 1];
+            const distance = distanceInMeters(lastBirdStep.location, birdStep.location);
+            const batteryUsed = lastBirdStep.battery_level - birdStep.battery_level;
+            const duration = birdStep.date - lastBirdStep.date;
+            const batteryUsedPerHour = batteryUsed / (duration / 60 / 60 / 1000);
+
+            // If the new bird location qualifies as a ride add it to the list
+            if (distance > 100 && duration > (2 * 60 * 1000) && batteryUsed > 0 && batteryUsedPerHour > 1) {
+                rides.push({
+                    birdCode, distance, duration, batteryUsed,
+                    formatedDuration: moment.duration(duration, "milliseconds").format("hh:mm:ss"),
+                    startTime: new Date(lastBirdStep.date),
+                    endTime: new Date(birdStep.date),
+                    startLocation: lastBirdStep.location,
+                    endLocation: birdStep.location,
+                    battery_level: birdStep.battery_level,
+                    shekels: 5 + (0.5 * Math.ceil(duration / 60000)),
+                    usd: 1 + (0.2 * Math.ceil(duration / 60000))
+                });
+            }
+        });
+    });
+
+    return rides;
+}
+
+const distanceInMeters = (p1, p2) => {
+    return geodist(
+        { lat: p1.latitude, lon: p1.longitude },
+        { lat: p2.latitude, lon: p2.longitude },
+        { unit: 'meters' }
+    );
+}
+
 module.exports = {
     createTimeline,
-    collectBirdLocations
+    collectBirdLocations,
+    collectBirdRides
 };
